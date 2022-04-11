d6be0ed4b09da042066f78fc85cdcf98227499b6
Rafael Luz <78448371+rafaoluz@users.noreply.github.com>
2022-04-11 16:03:02 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+if(window.SimpleSlide) {
+    new SimpleSlide({
+        slide: "info_aulas",
+        time: 5000,
+        nav:true
+    });
+
+}
+
+
+if(window.SimpleAnime) {
+    new SimpleAnime()
+}
+
+
